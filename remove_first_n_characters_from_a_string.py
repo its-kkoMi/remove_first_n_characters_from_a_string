e512ad4ef5e3524e68435c12c393609ac46745eb
James Mikko P. Recario <jamesmikkoprecario@iskolarngbayan.pup.edu.ph>
2024-01-26 00:36:38 +0800

    Added a user input for a word and a number

--- remove_first_n_characters_from_a_string.py
+++ remove_first_n_characters_from_a_string.py
@@ -2,5 +2,9 @@
 # Write a program to remove characters from a string starting from zero up to n and return a new string.
 
 # Ask user for input (word and number)
-# Remove character
+
+word = str(input("Enter a word: "))
+number_of_characters = int(input(f"Enter a number from 0 to {len(word)}: "))
+
+# Remove character using def
 # Print new string
