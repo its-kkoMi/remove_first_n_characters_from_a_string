1c91b25f4a33f98e3ce024df82b3d0bc2911a32e
James Mikko P. Recario <jamesmikkoprecario@iskolarngbayan.pup.edu.ph>
2024-01-26 00:33:47 +0800

    Program Instructions and Pseudocode

---
+++ remove_first_n_characters_from_a_string.py
@@ -0,0 +1,6 @@
+# Assignment 5 - Exercise 4
+# Write a program to remove characters from a string starting from zero up to n and return a new string.
+
+# Ask user for input (word and number)
+# Remove character
+# Print new string
